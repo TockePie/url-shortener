663d257c4a689dd6c97fbb2b163e640c0b63ceb2
Maxim <144056146+TockePie@users.noreply.github.com>
2025-12-19 14:27:34 +0200

    setup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
@@ -8,6 +9,8 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   const port = configService.get<number>('PORT') ?? 3000
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
   await app.listen(port, () => {
     console.log(`The server is running on: http://localhost:${port}`)
